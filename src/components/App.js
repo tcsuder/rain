0daf26e13c825c64958dd0acd3ddfd4b790c56b5
tcsuder@gmail.com <tcsuder@gmail.com>
2020-06-09 14:37:10 -0700

    set up basic drops with splash and puddle anmation

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,38 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import './styles.scss';
 
 const App = () => {
   const [drops, setDrops] = useState(5);
   const rain = Array.apply(null, new Array(drops));
+  const droplets = Array.apply(null, new Array(3));
 
   return (
     <div className="App">
       <div className="sky">
-        {rain.map((_, idx) => (
-          <div key={idx} className="drop" />
+        {rain.map((_, i) => (
+          <div key={i} className="drop" />
+        ))}
+      </div>
+      <div className="ground">
+        {rain.map((_, i) => (
+          <div key={i} className="splash">
+            {droplets.map((_, j) => (
+              <div key={j} className="droplet" />
+            ))}
+          </div>
         ))}
       </div>
       <div
         className="earth"
         onClick={() => {
-          setDrops(drops + 10);
+          if (drops < 100) {
+            setDrops(drops + 10);
+          } else {
+            setDrops(5);
+          }
         }}>
-        {rain.map((_, idx) => (
-          <div key={idx} className="puddle" />
+        {rain.map((_, i) => (
+          <div key={i} className="puddle" />
         ))}
       </div>
     </div>
